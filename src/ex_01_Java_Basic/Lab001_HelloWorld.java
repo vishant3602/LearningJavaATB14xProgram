c6923a503339eaf8d4001e7cb19bcdb56c6770bb
vishant3602 <vishantkhatri@gmail.com>
2025-09-12 20:58:01 +0530

    Lab001 - Hello World printing automation

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World test");
     }
 }
